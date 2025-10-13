c185be2b71077b017573e2a4f65f7da5c2a6a564
nady4 <dev@nady4.com>
2025-10-13 06:06:36 -0300

    upsert seed

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,24 +1,27 @@
 import { PrismaClient } from "@prisma/client";
+import products from "../src/data/products.json" assert { type: "json" };
+
 const prisma = new PrismaClient();
-import products from "../src/data/products.json" with { type: "json" };
 
 async function main() {
-    console.log("🌱 Seeding products...");
-  
-    for (const product of products) {
-      await prisma.product.create({
-        data: product,
-      });
-    }
-  
-    console.log("✅ Seeding complete!");
-  }
-  
-  main()
-    .catch((error) => {
-      console.error("❌ Error seeding database:", error);
-      process.exit(1);
-    })
-    .finally(async () => {
-      await prisma.$disconnect();
+  console.log("🌱 Seeding products...");
+
+  for (const product of products) {
+    await prisma.product.upsert({
+      where: { id: product.id },
+      update: {},
+      create: product,
     });
+  }
+
+  console.log("✅ Seeding complete!");
+}
+
+main()
+  .catch((error) => {
+    console.error("❌ Error seeding database:", error);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
